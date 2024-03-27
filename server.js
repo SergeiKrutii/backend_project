e5a6db0fd700845c11543828e6e40539c7960057
SergeiKrutii <106181150+SergeiKrutii@users.noreply.github.com>
2024-03-27 23:34:39 +0200

    fix connect to server#5

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+const app = require("./app");
 const mongoose = require("mongoose");
-require("dotenv").config();
-
 const { DB_HOST } = process.env;
+const { PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    console.log("Database connection successful");
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log("Database connection successful");
+    });
   })
   .catch((error) => {
     console.log(error.message);
