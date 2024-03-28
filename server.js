322c038ac0b3d1488b5773329851976ce72c6334
SergeiKrutii <106181150+SergeiKrutii@users.noreply.github.com>
2024-03-28 23:20:58 +0200

    fix connect to server#9

--- server.js
+++ server.js
@@ -12,24 +12,12 @@ const connectToDatabase = async () => {
     console.log("Database connection successful");
   } catch (error) {
     console.error("Error connecting to database:", error.message);
-    process.exit(1); // Выход из процесса Node.js в случае ошибки подключения к базе данных
+    process.exit(1);
   }
 };
 
-connectToDatabase(); // Подключаемся к базе данных
+connectToDatabase();
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// mongoose
-//   .connect(MONGOHOST, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     app.listen(port, "0.0.0.0", () => {
-//       console.log("Database connection successful");
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error.message);
-//     process.exit(1);
-//   });
