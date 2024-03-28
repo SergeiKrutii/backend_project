e18abc01d25f69c54f6597aea9d6800ae2d743a0
SergeiKrutii <106181150+SergeiKrutii@users.noreply.github.com>
2024-03-28 22:44:38 +0200

    fix connect to server#7

--- server.js
+++ server.js
@@ -3,14 +3,35 @@ const mongoose = require("mongoose");
 const { MONGOHOST } = process.env;
 const port = process.env.PORT || 3001;
 
-mongoose
-  .connect(MONGOHOST)
-  .then(() => {
-    app.listen(port, "0.0.0.0", () => {
-      console.log("Database connection successful");
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(MONGOHOST, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
     });
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
+    console.log("Database connection successful");
+  } catch (error) {
+    console.error("Error connecting to database:", error.message);
+    process.exit(1); // Выход из процесса Node.js в случае ошибки подключения к базе данных
+  }
+};
+
+connectToDatabase(); // Подключаемся к базе данных
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// mongoose
+//   .connect(MONGOHOST, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     app.listen(port, "0.0.0.0", () => {
+//       console.log("Database connection successful");
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
