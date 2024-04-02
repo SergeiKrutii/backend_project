0736142cb9c0c9f034b4ba17caec2b854e003d6b
SergeiKrutii <106181150+SergeiKrutii@users.noreply.github.com>
2024-04-03 01:44:53 +0300

    fix register bug#1

--- controllers/books/getAllUserBooks.js
+++ controllers/books/getAllUserBooks.js
@@ -1,9 +1,14 @@
+const { HttpError } = require("../../helpers");
 const { Book } = require("../../models");
 
 const getAllUserBook = async (req, res) => {
   const { _id: user } = req.user;
 
+  if (!user) throw HttpError(404);
+
   const result = await Book.find({ user });
+
+  if (!result) throw HttpError(403);
 
   res.json(result);
 };
